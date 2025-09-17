1a42b282ebbf5fe0d1a80f16ce2dd7b424c80e6b
Nicole0509 <niashavril0509@gmail.com>
2025-09-17 10:38:23 +0200

    Invoking the rectangle and displaying the area of the passed arguments

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
 
         System.out.println("Welcome To Shapes In OOP!");
 
+        Circle circle = new Circle(2);
+
+        Rectangle rectangle = new Rectangle(4, 5);
+
+        System.out.printf("The area of the circle is: %.2f cm²\n" , rectangle.area());
+
     }
 }
