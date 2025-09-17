7a520cb655ba0a345da26a338f57f1a8f9faa84b
Nicole0509 <niashavril0509@gmail.com>
2025-09-17 10:31:55 +0200

    Added a construct to rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -3,6 +3,11 @@ public class Rectangle extends Shape {
     public double width;
     public double length;
 
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
     @Override
     public double area() {
         return length * width;
